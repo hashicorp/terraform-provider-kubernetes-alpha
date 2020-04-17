f3352d3fd3519c48fba3757edf2614bd564a141b
Alex Somesan <alex.somesan@gmail.com>
2020-04-17 02:19:13 +0200

    deployment example - fix resource name

--- examples/deployment-hcl/deployment.tf
+++ examples/deployment-hcl/deployment.tf
@@ -1,7 +1,7 @@
 provider "kubernetes-alpha" {
 }
 
-resource "kubernetes_manifest_hcl" "test-pod" {
+resource "kubernetes_manifest_hcl" "test-deployment" {
   provider = kubernetes-alpha
 
   manifest = {
