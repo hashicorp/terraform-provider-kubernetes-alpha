1a2a136f61fa219670910a6aee900193d178ab15
John Houston <jhouston@hashicorp.com>
2020-05-04 22:23:19 -0400

    Use gox for cross compiling

--- scripts/build_release_binaries.sh
+++ scripts/build_release_binaries.sh
@@ -7,20 +7,16 @@ if [ -z "$VERSION" ]; then
     exit 1
 fi
 
-OS_ARCH="darwin,amd64 freebsd,386 freebsd,amd64 freebsd,arm linux,386 linux,amd64 linux,arm openbsd,386 openbsd,amd64 solaris,amd64 windows,386 windows,amd64"
-ASSETS_DIR="release-bin"
+OS_ARCH="darwin/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm openbsd/386 openbsd/amd64 solaris/amd64 windows/386 windows/amd64"
+ASSETS_DIR="./release-bin"
 
 mkdir -p $ASSETS_DIR
 rm -rf $ASSETS_DIR/*
 
-for osarch in $OS_ARCH; do
-    OS=$(echo "$osarch" | cut -d, -f1)
-    ARCH=$(echo "$osarch" | cut -d, -f2)
-    FILENAME="terraform-provider-kubernetes-alpha_${VERSION}_${OS}_${ARCH}"
-    GOOS=$OS GOARCH=$ARCH go build -o $ASSETS_DIR/$FILENAME
-    ZIPFILENAME="$ASSETS_DIR/$FILENAME.zip"
-    zip -q $ZIPFILENAME $ASSETS_DIR/$FILENAME
-    rm -f $ASSETS_DIR/$FILENAME
-    echo $ZIPFILENAME
+gox -osarch "$OS_ARCH" -output "$ASSETS_DIR/{{.Dir}}_${VERSION}_{{.OS}}_{{.Arch}}"
+
+for f in $ASSETS_DIR/*; do
+    zip -q "$f.zip" $f
+    rm -f $f
 done
 
