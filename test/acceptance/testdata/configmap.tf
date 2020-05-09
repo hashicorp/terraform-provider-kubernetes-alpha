41e2b82e79b6fddbd773f221a7c1b99dde3042cf
John Houston <jhouston@hashicorp.com>
2020-05-09 13:31:16 -0400

    remove superfluous quotes from test configs

--- test/acceptance/testdata/configmap.tf
+++ test/acceptance/testdata/configmap.tf
@@ -2,14 +2,14 @@ resource "kubernetes_manifest" "test" {
   provider = kubernetes-alpha
 
   manifest = {
-    "apiVersion" = "v1"
-    "kind" = "ConfigMap"
-    "metadata" = {
-	  "name" = var.name
-	  "namespace" = var.namespace	
+    apiVersion = "v1"
+    kind = "ConfigMap"
+    metadata = {
+	  name = var.name
+	  namespace = var.namespace	
     }
-    "data" = {
-      "foo" = "bar"
+    data = {
+      foo = "bar"
     }
   }
 }
--- test/acceptance/testdata/configmap_modified.tf
+++ test/acceptance/testdata/configmap_modified.tf
@@ -2,21 +2,21 @@ resource "kubernetes_manifest" "test" {
   provider = kubernetes-alpha
 
   manifest = {
-    "apiVersion" = "v1"
-    "kind" = "ConfigMap"
-    "metadata" = {
-	  "name" = var.name
-	  "namespace" = var.namespace
-	  "annotations" = {
-		"test" = "1"
+    apiVersion = "v1"
+    kind = "ConfigMap"
+    metadata = {
+	  name = var.name
+	  namespace = var.namespace
+	  annotations = {
+		test = "1"
 	  }
-	  "labels" = {
-	    "test" = "2"
+	  labels = {
+	    test = "2"
 	  }
     }
-    "data" = {
-	  "foo" = "bar"
-	  "fizz" = "buzz"
+    data = {
+	  foo = "bar"
+	  fizz = "buzz"
     }
   }
 }
