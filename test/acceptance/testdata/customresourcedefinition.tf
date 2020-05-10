63c7c9dd671179a359feb9b029ef18850c6d5281
John Houston <jhouston@hashicorp.com>
2020-05-09 22:07:50 -0400

    Add acctest for CustomResourceDefinition

---
+++ test/acceptance/testdata/customresourcedefinition.tf
@@ -0,0 +1,41 @@
+provider "kubernetes-alpha" {
+  server_side_planning = var.server_side_planning
+}
+
+resource "kubernetes_manifest" "test" {
+  provider = kubernetes-alpha
+
+  manifest = {
+    apiVersion = "apiextensions.k8s.io/v1"
+    kind = "CustomResourceDefinition"
+    metadata = {
+      name = "${var.plural}.${var.group}"
+    }
+    spec = {
+      group = var.group
+      names = {
+        kind = var.kind
+        plural = var.plural
+      }
+      scope = "Namespaced"
+      versions = [{
+        name = var.group_version
+        served = true
+        storage = true
+        schema = {
+          openAPIV3Schema = {
+            type = "object"
+            properties = {
+              data = {
+                type = "string"
+              }
+              refs = {
+                type = "number"
+              }
+            }
+          }
+        }
+      }]
+    }
+  }
+}
