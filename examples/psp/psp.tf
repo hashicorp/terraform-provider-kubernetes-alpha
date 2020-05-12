ca2b183f17fbaceb3152c271081df748f0cab0fb
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-05-12 17:24:00 -0400

    Include PodSecurityPolicy & Vault Example (#11)
    * Include PodSecurityPolicy example
    
    * Add Vault example
    
    * rename examples
    
    * Rename kubernetes_manifest resource
    
    Co-authored-by: Aareet Shermon <aareet@users.noreply.github.com>

---
+++ examples/psp/psp.tf
@@ -0,0 +1,37 @@
+variable "kubeconfig" {}
+
+provider "kubernetes-alpha" {
+  config_file          = var.kubeconfig
+  server_side_planning = true
+}
+
+# PodSecurityPolicy only works on Kubernetes 1.17+
+resource "kubernetes_manifest" "psp" {
+  provider = kubernetes-alpha
+  manifest = {
+    "apiVersion" = "policy/v1beta1"
+    "kind"       = "PodSecurityPolicy"
+    "metadata" = {
+      "name"      = "example"
+      "namespace" = "default"
+    }
+    "spec" = {
+      "fsGroup" = {
+        "rule" = "RunAsAny"
+      }
+      "privileged" = false
+      "runAsUser" = {
+        "rule" = "RunAsAny"
+      }
+      "seLinux" = {
+        "rule" = "RunAsAny"
+      }
+      "supplementalGroups" = {
+        "rule" = "RunAsAny"
+      }
+      "volumes" = [
+        "*",
+      ]
+    }
+  }
+}
