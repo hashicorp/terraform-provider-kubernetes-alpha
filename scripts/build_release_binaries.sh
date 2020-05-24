09ba5bba7dda8a629051559bbba22fb5ac9c3e92
John Houston <jhouston@hashicorp.com>
2020-05-24 18:50:58 -0400

    Add codesigning for windows and darwin binaries Add nightly github action to trigger build

--- scripts/build_release_binaries.sh
+++ scripts/build_release_binaries.sh
@@ -7,8 +7,10 @@ if [ -z "$VERSION" ]; then
     exit 1
 fi
 
-OS_ARCH="darwin/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm openbsd/386 openbsd/amd64 solaris/amd64 windows/386 windows/amd64"
+# FIXME replace this with goreleaser
+OS_ARCH="freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm openbsd/386 openbsd/amd64"
 ASSETS_DIR="./release-bin"
+BINARY_NAME="terraform-provider-kubernetes-alpha"
 
 mkdir -p $ASSETS_DIR
 rm -rf $ASSETS_DIR/*
@@ -16,7 +18,22 @@ rm -rf $ASSETS_DIR/*
 gox -osarch "$OS_ARCH" -output "$ASSETS_DIR/{{.Dir}}_${VERSION}_{{.OS}}_{{.Arch}}"
 
 for f in $ASSETS_DIR/*; do
-    zip -q -j "$f.zip" $f
-    rm -f $f
+    mv $f $ASSETS_DIR/$BINARY_NAME
+    zip -q -j "$f.zip" $ASSETS_DIR/$BINARY_NAME
+    rm -f $ASSETS_DIR/$BINARY_NAME
 done
 
+
+echo "Building and signing darwin binary"
+GOOS=darwin GOARCH=amd64 go build 
+hc-codesign sign -product-name=${BINARY_NAME} ./${BINARY_NAME}
+zip -q -j "${ASSETS_DIR}/${BINARY_NAME}_${VERSION}_darwin_amd64.zip" ${BINARY_NAME}
+
+echo "Building and signing windows binaries"
+GOOS=windows GOARCH=amd64 go build 
+hc-codesign sign -product-name=${BINARY_NAME} ./${BINARY_NAME}.exe
+zip -q -j "${ASSETS_DIR}/${BINARY_NAME}_${VERSION}_windows_amd64.zip" ${BINARY_NAME}.exe
+
+GOOS=windows GOARCH=386 go build 
+hc-codesign sign -product-name=${BINARY_NAME} ./${BINARY_NAME}.exe
+zip -q -j "${ASSETS_DIR}/${BINARY_NAME}_${VERSION}_windows_386.zip" ${BINARY_NAME}.exe
