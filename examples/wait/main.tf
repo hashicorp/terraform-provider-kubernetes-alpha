c76512254204d1ffda84cfc1d6d4f774d78f1007
John Houston <jhouston@hashicorp.com>
2020-08-10 15:06:01 -0400

    Add wait_for example

---
+++ examples/wait/main.tf
@@ -0,0 +1,37 @@
+provider "kubernetes-alpha" {
+  config_path = "~/.kube/config"
+  server_side_planning = true
+}
+
+resource "kubernetes_manifest" "example" {
+  provider = kubernetes-alpha
+
+  manifest = {
+    apiVersion = "v1"
+    kind       = "Pod"
+
+    metadata = {
+      name      = "example-pod"
+      namespace = "default"
+
+      labels = {
+        app = "nginx"
+      }
+    }
+
+    spec = {
+      containers = [
+        {
+          image = "nginx:1.19"
+          name  = "nginx"
+        },
+      ]
+    }
+  }
+
+  wait_for {
+    fields = {
+      "status.phase" = "Running"
+    }
+  }
+}
