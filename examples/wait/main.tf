774db788406aeeeee224a942b40288393ef0414f
John Houston <jhouston@hashicorp.com>
2020-08-19 10:35:41 -0400

    Add more fields to example

--- examples/wait/main.tf
+++ examples/wait/main.tf
@@ -25,7 +25,7 @@ resource "kubernetes_manifest" "example" {
           image = "nginx:1.19"
 
           readinessProbe = {
-            initialDelaySeconds = 5
+            initialDelaySeconds = 10
 
             httpGet = {
               path = "/"
@@ -39,7 +39,10 @@ resource "kubernetes_manifest" "example" {
 
   wait_for = {
     fields = {
-      "status.containerStatuses.0.ready" = "true"
+      "status.containerStatuses.0.ready"        = "true",
+      "status.containerStatuses.0.restartCount" = "0",
+      "status.podIP"                            = "^(\\d+(\\.|$)){4}",
+      "status.phase"                            = "Running",
     }
   }
 }
