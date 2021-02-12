0516a33043b16bbb8b0494c9d7fd57284c096a3f
Alex Somesan <alex.somesan@gmail.com>
2021-02-12 17:11:05 +0100

    Simplistic validation for absent / unexpected manifest attributes

--- provider/validate.go
+++ provider/validate.go
@@ -2,16 +2,22 @@ package provider
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5/tftypes"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 // ValidateResourceTypeConfig function
 func (s *RawProviderServer) ValidateResourceTypeConfig(ctx context.Context, req *tfprotov5.ValidateResourceTypeConfigRequest) (*tfprotov5.ValidateResourceTypeConfigResponse, error) {
 	resp := &tfprotov5.ValidateResourceTypeConfigResponse{}
+	validKeys := map[string]bool{
+		"apiVersion": false,
+		"kind":       false,
+		"metadata":   false,
+		"spec":       false,
+	}
+
 	rt, err := GetResourceType(req.TypeName)
 	if err != nil {
 		resp.Diagnostics = append(resp.Diagnostics, &tfprotov5.Diagnostic{
@@ -33,6 +39,8 @@ func (s *RawProviderServer) ValidateResourceTypeConfig(ctx context.Context, req 
 		return resp, nil
 	}
 
+	att := tftypes.AttributePath{}.WithAttributeName("manifest")
+
 	configVal := make(map[string]tftypes.Value)
 	err = config.As(&configVal)
 	if err != nil {
@@ -46,7 +54,6 @@ func (s *RawProviderServer) ValidateResourceTypeConfig(ctx context.Context, req 
 
 	manifest, ok := configVal["manifest"]
 	if !ok {
-		att := tftypes.AttributePath{}.WithAttributeName("manifest")
 		resp.Diagnostics = append(resp.Diagnostics, &tfprotov5.Diagnostic{
 			Severity:  tfprotov5.DiagnosticSeverityError,
 			Summary:   "Manifest missing from resource configuration",
@@ -55,20 +62,44 @@ func (s *RawProviderServer) ValidateResourceTypeConfig(ctx context.Context, req 
 		})
 		return resp, nil
 	}
-	manObj, err := TFValueToUnstructured(manifest, tftypes.AttributePath{})
+
+	rawManifest := make(map[string]tftypes.Value)
+	err = manifest.As(&rawManifest)
 	if err != nil {
-		att := tftypes.AttributePath{}.WithAttributeName("manifest")
 		resp.Diagnostics = append(resp.Diagnostics, &tfprotov5.Diagnostic{
 			Severity:  tfprotov5.DiagnosticSeverityError,
-			Summary:   `Invalid "manifest" attribute`,
+			Summary:   `Failed to extract "manifest" attribute value from resource configuration`,
 			Detail:    err.Error(),
 			Attribute: &att,
 		})
 		return resp, nil
 	}
-	manu := unstructured.Unstructured{Object: manObj.(map[string]interface{})}
 
-	s.logger.Trace("[ValidateResourceTypeConfig]", "[Manifest]", spew.Sdump(manu))
+	for key := range rawManifest {
+		_, ok := validKeys[key]
+		if !ok {
+			kp := att.WithAttributeName(key)
+			resp.Diagnostics = append(resp.Diagnostics, &tfprotov5.Diagnostic{
+				Severity:  tfprotov5.DiagnosticSeverityError,
+				Summary:   `Invalid attribute key in "manifest" value`,
+				Detail:    fmt.Sprintf("'%s' is not a valid attribute key in a manifest configuration", key),
+				Attribute: &kp,
+			})
+		} else {
+			validKeys[key] = true
+		}
+	}
+	for key, present := range validKeys {
+		if !present {
+			kp := att.WithAttributeName(key)
+			resp.Diagnostics = append(resp.Diagnostics, &tfprotov5.Diagnostic{
+				Severity:  tfprotov5.DiagnosticSeverityError,
+				Summary:   `Required attribute key missing from "manifest" value`,
+				Detail:    fmt.Sprintf("'%s' attribute key is missing from manifest configuration", key),
+				Attribute: &kp,
+			})
+		}
+	}
 
 	return resp, nil
 }
